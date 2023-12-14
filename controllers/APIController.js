9feb45c0d4605097702c18db4e33b55f4f29eb75
duongxuannhan2002 <sieuxaya37.na@gmail.com>
2023-12-14 12:38:42 +0700

    n

--- controllers/APIController.js
+++ controllers/APIController.js
@@ -48,46 +48,42 @@ export const postUser = async (req, res) => {
     let name = req.body.name
     let email = req.body.email
     let phoneNumber = req.body.phoneNumber
-    let pass
-    if (!email || !name || !req.body.pass || !phoneNumber) {
+    let pass = req.body.pass
+    if (!email || !name || !pass || !phoneNumber) {
         return res.status(200).json({
             message: 'oh NOOOOOO'
         })
     }
+    bcrypt.hash(pass, 5, async function (err, hash) {
+        if (err) {
+            console.error(err);
+            return;
+        }
 
-    while (pass == null) {
-        bcrypt.hash(req.body.pass, 5, async function (err, hash) {
-            if (err) {
-                console.error(err);
-                return;
-            }
-
-            // Lưu trữ hash mật khẩu trong cơ sở dữ liệu
-            pass = hash
-            try {
-                let results = await checkPhoneNumber(phoneNumber)
-                connection.release;
-                if (results.length > 0) {
+        // Lưu trữ hash mật khẩu trong cơ sở dữ liệu
+        try {
+            let results = await checkPhoneNumber(phoneNumber)
+            connection.release;
+            if (results.length > 0) {
+                return res.status(200).json({
+                    message: 'Số điện thoại đã được đăng ký'
+                })
+            } else {
+                try {
+                    console.log(pass)
+                    await createUser(name, email, hash, phoneNumber)
+                    connection.release;
                     return res.status(200).json({
-                        message: 'Số điện thoại đã được đăng ký'
+                        message: 'ok men',
                     })
-                } else {
-                    try {
-                        console.log(pass)
-                        await createUser(name, email, pass, phoneNumber)
-                        connection.release;
-                        return res.status(200).json({
-                            message: 'ok men',
-                        })
-                    } catch (error) {
-                        res.status(409).json({ message: error.message });
-                    }
+                } catch (error) {
+                    res.status(409).json({ message: error.message });
                 }
-            } catch (error) {
-                res.status(409).json({ message: error.message });
             }
-        });
-    }
+        } catch (error) {
+            res.status(409).json({ message: error.message });
+        }
+    });
 }
 
 export const putUser = async (req, res) => {
